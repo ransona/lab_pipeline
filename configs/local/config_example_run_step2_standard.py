e0cb2bef83b91c09800549ddfcbe07103ac60fc2
SI2 <SI2@example.com>
2026-06-03 17:42:07 +0200

    Update local step2 test config

--- configs/local/config_example_run_step2_standard.py
+++ configs/local/config_example_run_step2_standard.py
@@ -9,9 +9,9 @@ step2_config = {}
 
 # Set userID to the local OS username that owns the local repository tree.
 step2_config["userID"] = "adamranson"
-step2_config["expIDs"] = ["2026-05-11_03_ESRC033"]
+step2_config["expIDs"] = ["2026-05-17_02_ESYB040"]
 step2_config["local_raw_repository_root"] = r"D:\data\Repository"
-step2_config["local_processed_repository_root"] = r"D:\processed\Repository"
+step2_config["local_processed_repository_root"] = r"F:\processed"
 step2_config["local_nas_repository_root"] = r"\\ar-lab-nas1\DataServer\Remote_Repository"
 step2_config["pre_secs"] = 5
 step2_config["post_secs"] = 5
